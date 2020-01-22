b1de63874458aafc8a82ef1c21ac68e812822252
ericpinhasovich <wildcats123>
2020-01-22 14:35:49 +0200

    added maps and apps

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import threading
 import time
 import requests
@@ -10,26 +10,26 @@ DB_FILE = "./database/settler.db"
 db = DB(DB_FILE)
 conn = db.conn
 
+users = []
+
 @app.route("/")
 def hello():
     return "Hello"
 
 
-@app.route("/signup", methods=['POST'])
+@app.route("/signup", methods=['GET'])
 def sign_up():
-    db.form_to_db("users", None, None)
-    # if requests.method == 'POST':
-    #     first_name = requests.form['first_name']
-    #     try:
-    #         db.form_to_db("users", None, None)
-    #     except Exception as e:
-    #         print(e)
     return render_template("signup.html")
 
 
-@app.route("/welcome", methods=['GET'])
+@app.route("/welcome", methods=['POST'])
 def welcome():
-    return render_template("welcome.html")
+    user = {
+    "id": users[-1]['id'] + 1,
+    "first_name": request.form['first_name'],
+    "last_name": request.form['last_name']
+    }
+    return render_template("welcome.html", user=user)
 
 
 @app.route("/map", methods=['GET'])
